524fcd9ae80bd6d911365debc64b18a1f8023215
damianfral <huevofritopamojarpan@gmail.com>
2014-02-28 17:06:43 +0100

    Meta keywords added.

--- site.hs
+++ site.hs
@@ -1,10 +1,10 @@
 -------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
-import           Control.Monad
-import           Text.Pandoc
-import           Text.Pandoc.Options
+import Data.Monoid
+import Control.Monad
+import Hakyll
+import Text.Pandoc
+import System.FilePath.Posix
 
 -------------------------------------------------------------------------------
 
@@ -44,13 +44,14 @@ main = hakyll $ do
 
 	match "fonts/*" $ do
 		route   idRoute
-		compile $ copyFileCompiler
+		compile copyFileCompiler
 
 	--match (fromList ["about.rst", "contact.markdown"]) $ do
 	--	route   $ setExtension "html"
 	--	compile $ pandocCompiler
 	--		>>= loadAndApplyTemplate "templates/default.html" defaultContext
 	--		>>= relativizeUrls
+
 
 	match "posts/*" $ do
 		route $ setExtension "html"
@@ -64,7 +65,7 @@ main = hakyll $ do
 	match "index.html" $ do
 		route idRoute
 		compile $ do
-			let indexCtx = field "posts" $ \_ -> postList $ recentFirst
+			let indexCtx = field "posts" $ const  $ postList recentFirst
 			getResourceBody
 				>>= applyAsTemplate indexCtx
 				>>= loadAndApplyTemplate "templates/default.html" postCtx
@@ -88,7 +89,7 @@ main = hakyll $ do
 	create ["atom.xml"] $ do
 		route idRoute
 		compile $ do
-			let feedCtx = postCtx `mappend` bodyField "description"
+			let feedCtx = postCtx <> bodyField "description"
 			posts <- fmap (take 10) . recentFirst =<<
 				loadAllSnapshots "posts/*" "content"
 			renderAtom myFeedConfiguration feedCtx posts
@@ -97,8 +98,7 @@ main = hakyll $ do
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-	dateField "date" "%d/%m/%Y" `mappend`
-	defaultContext
+	dateField "date" "%d/%m/%Y" <> defaultContext <> metaKeywordContext
 
 
 --------------------------------------------------------------------------------
@@ -106,9 +106,28 @@ postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
 postList sortFilter = do
 	posts   <- sortFilter =<< loadAll "posts/*"
 	itemTpl <- loadBody "templates/post-item.html"
-	list    <- applyTemplateList itemTpl postCtx posts
-	return list
+	applyTemplateList itemTpl postCtx posts
+
+niceRoute :: Routes
+niceRoute = customRoute createIndexRoute
+	where
+		createIndexRoute ident = takeBaseName (toFilePath ident) </> "index.html"
 
 
 stylusCompiler :: Compiler (Item String)
-stylusCompiler = getResourceString >>= withItemBody  (unixFilter "stylus" ["-p", "-u nib"]) >>= return . fmap compressCss
+stylusCompiler = liftM (fmap compressCss) $ getResourceString >>= withItemBody  (unixFilter "stylus" ["-p", "-u nib"])
+
+-- metaKeywordContext will return a Context containing a String
+metaKeywordContext :: Context String
+-- can be reached using $metaKeywords$ in the templates
+-- Use the current item (markdown file)
+metaKeywordContext = field "metaKeywords" $ \item -> do
+	-- tags contains the content of the "tags" metadata
+	-- inside the item (understand the source)
+	tags <- getMetadataField (itemIdentifier item) "tags"
+	-- if tags is empty return an empty string
+	-- in the other case return
+	--   <meta name="keywords" content="$tags$">
+	return $ maybe "" showMetaTags tags
+		where
+			showMetaTags t = "<meta name=\"keywords\" content=\"" ++ t ++ "\">\n"
